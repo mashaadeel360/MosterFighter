1e2d27da315639049ca368c2aa50bce3b53a8477
Aadeel Shah <aadeel.shah@meridianstudents.org>
2021-09-21 11:58:44 -0500

    init

--- src/Monster.java
+++ src/Monster.java
@@ -1,27 +1,64 @@
 public class Monster {
-    private String name;
-    private int health;
-    private int strength;
+    String name;
+    int health = 400; 
+    int strength;
 
     public Monster(String name) {
         this.name = name;
-        this.health = 10;
+        this.health = 200;
         this.strength = 3;
     }
 
     public void takeDamage(int damage) {
         health-=damage;
-    }    
-
-    public void attack(Monster target) {
-        int damage = (int) (Math.random() * strength);
-        System.out.println(name + "attacks!!");
-        target.takeDamage(damage);
     }
 
     public boolean isAlive () {
         return health > 0;
     }
-        
-        
+
+//this game should be like pokemon. Player one choses a move, it does that move, health subtract from opponant, then opponent goes. this goes on till one wins.
+
+//Player choses move
+//move 4 heals +10 everytime it is used
+//1) thunderpunch = 75, 2) megakick = 120, 3) thunderbolt = 90, 4) risingvoltage = +10 hp
+public void move(Monster target, int option) {
+    switch (option){
+        case 1: System.out.println("You Chose thunderpunch which dealt 70 damage"); target.takeDamage(70); break;
+        case 2: System.out.println("You Chose megakick which dealt 100 damage"); target.takeDamage(100); break;
+        case 3: System.out.println("You Chose thunderbolt which dealt 80 damage"); target.takeDamage(80); break;
+        case 4: System.out.println("You Chose risingvoltage which added 10 hp"); this.takeDamage(-10); break;
+    }
+    checkHealth();
+}
+
+//Computer will take damage according to the player attack and will randomly choose an attack
+// 1) dragonbreath = 60, 2) dragonclaw = 80, 3) flamthrower = 90, 4)flarebits = 120, 5)firedoom = +30 hp
+
+    public void computerMove (Monster target){
+        int randomNumber = (int) (Math.random() * 4) + 1;
+        System.out.println(randomNumber);
+        switch (randomNumber){
+            case 1: System.out.println("Computer Chose dragonbreath which dealt 70 damage"); target.takeDamage(70); break;
+            case 2: System.out.println("Computer Chose dragonclaw which dealt 80 damage"); target.takeDamage(80); break;
+            case 3: System.out.println("Computer Chose flamethrower which dealt 90 damage"); target.takeDamage(90); break;
+            case 4: System.out.println("Computer Chose flarebits which dealt 130 damage"); target.takeDamage(130); break;
+            case 5: System.out.println("Computer Chose firedoom which added 30 hp to itself"); this.takeDamage(-30); break;
+        }
+        target.checkHealth();
+    }
+
+//Player health
+//player health should equal to 200 and not excede 200 especealy after healing
+
+    public void checkHealth(){
+       if (health >= 200)
+           health = 200; 
+    }
+
+    public void Display(){
+        System.out.println(name+" is at "+health+" hp");
+
+    
+    }
 }
--- src/MonsterFighter.java
+++ src/MonsterFighter.java
@@ -1,13 +1,46 @@
-public class MonsterFighter {
-    public static void main(String[] args) throws Exception {
-        Monster player = new Monster("peekandchew");
-        Monster computer = new Monster("charsalad");
+import java.util.Scanner;
 
-        int roundCounter = 0;
-        while(player.isAlive() && computer.isAlive()) {
-            if(roundCounter % 2 == 0) player.attack(computer);
-            else computer.attack(player);
-            roundCounter++;
-        }
+public class MonsterFighter {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        boolean gameEnd = false;
+        do{
+            Monster player = new Monster("Peekandchew(You)");
+            Monster computer = new Monster("Chardsalad(Computer)");
+            
+            int roundCounter = 0;
+            while(player.isAlive() && computer.isAlive()) {
+                if(roundCounter % 2 == 0) {
+                    System.out.println("\nchoose your attack: 1) thunderpunch = 75, 2) megakick = 120, 3) thunderbolt = 90, 4) risingvoltage = +10");
+                    int userOption = sc.nextInt();
+                    player.move(computer, userOption);
+                    System.out.println("");
+                    player.Display();
+                    computer.Display();
+                }
+                else {
+                    computer.computerMove(player);
+                    System.out.println("");
+                    player.Display();
+                    computer.Display();
+                }
+                Thread.sleep(2000);
+                roundCounter++;
+            }
+            if (player.isAlive()) {
+                System.out.println("\nYou Kicked the computer's ass and won the game, good job");
+            }
+            else {
+                System.out.println("\nThe computer beat your ass and won the game");
+            }
+
+            System.out.println("Do you want to play again? \n(Y or N)");
+            sc.nextLine();
+            String playerOption = sc.nextLine();
+            if(!"Y".equals(playerOption.toUpperCase())){
+                gameEnd = true;
+            }
+        }while(!gameEnd);
+        sc.close();
     }
 }
